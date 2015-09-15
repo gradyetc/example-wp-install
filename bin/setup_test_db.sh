9662a4c72cc7a983c4f99fb8cc23f42fc427028b
Mike Burns <mike.burns@upstatement.com>
2015-09-15 17:52:52 -0400

    Fix TravisCI DB setup errors

--- bin/setup_test_db.sh
+++ bin/setup_test_db.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-mysql -u $WORDPRESS_DB_USER --password=$WORDPRESS_DB_PASS -h $WORDPRESS_DB_HOST -P $WORDPRESS_DB_PORT $WORDPRESS_DB_NAME < ./test/db/wordpress.sql
+echo "Importing smoke test install..."
+echo "DB User: $WORDPRESS_DB_USER"
+echo "DB Password: $WORDPRESS_DB_PASS"
+echo "DB Host: $WORDPRESS_DB_HOST"
+echo "DB Name: $WORDPRESS_DB_NAME"
+
+mysql -u "$WORDPRESS_DB_USER" --password="$WORDPRESS_DB_PASS" -h "$WORDPRESS_DB_HOST" -P "$WORDPRESS_DB_PORT" "$WORDPRESS_DB_NAME" < ./test/db/wordpress.sql
 
