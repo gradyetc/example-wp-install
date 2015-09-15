c799183d07399097ccd84e1a005e05b677b5228c
Mike Burns <mike.burns@upstatement.com>
2015-09-15 18:14:38 -0400

    More TravisCI debug lines...

--- bin/setup_test_db.sh
+++ bin/setup_test_db.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 echo "Importing smoke test install..."
+
 echo "DB User: $WORDPRESS_DB_USER"
 echo "DB Password: $WORDPRESS_DB_PASS"
 echo "DB Host: $WORDPRESS_DB_HOST"
 echo "DB Name: $WORDPRESS_DB_NAME"
+
+dig example-install.dev
+curl -v http://example-install.dev
 
 mysql -u "$WORDPRESS_DB_USER" --password="$WORDPRESS_DB_PASS" -h "$WORDPRESS_DB_HOST" -P "$WORDPRESS_DB_PORT" "$WORDPRESS_DB_NAME" < ./test/db/wordpress.sql
 
