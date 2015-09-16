e788c639121261e81845244f66c7aad8aac2ccc3
Mike Burns <mike.burns@upstatement.com>
2015-09-15 20:57:52 -0400

    Cleanup Travis scripts and Docker files

--- bin/setup_test_db.sh
+++ bin/setup_test_db.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-echo "Importing smoke test install..."
+set -ev
 
-echo "DB User: $WORDPRESS_DB_USER"
-echo "DB Password: $WORDPRESS_DB_PASS"
-echo "DB Host: $WORDPRESS_DB_HOST"
-echo "DB Name: $WORDPRESS_DB_NAME"
+echo "Importing smoke test install..."
 
 mysql -u "$WORDPRESS_DB_USER" --password="$WORDPRESS_DB_PASS" -h "$WORDPRESS_DB_HOST" -P "$WORDPRESS_DB_PORT" "$WORDPRESS_DB_NAME" < ./test/db/wordpress.sql
 
